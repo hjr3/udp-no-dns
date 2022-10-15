d6565f1682654a033d572841087f67e1df2d3666
Herman J. Radtke III <herman@hermanradtke.com>
2022-10-15 16:44:41 -0400

    Initial commit

---
+++ udp.mjs
@@ -0,0 +1,17 @@
+import dgram from 'node:dgram';
+import { Buffer } from 'node:buffer';
+import util from 'util';
+
+const host = 'www.example.com';
+
+const socket = dgram.createSocket('udp4');
+socket.send2 = util.promisify(socket.send);
+
+const msg = Buffer.from('foo');
+
+await socket.send2(msg, 0, msg.length, 8125, host);
+console.log('msg 1 sent');
+await socket.send2(msg, 0, msg.length, 8125, host);
+console.log('msg 2 sent');
+
+socket.close();
