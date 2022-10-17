e2301d851621940158fd0e90be0d58e643b42e23
Herman J. Radtke III <herman@hermanradtke.com>
2022-10-17 08:16:50 -0400

    properly cache ip address

--- udp.mjs
+++ udp.mjs
@@ -2,6 +2,7 @@ import dgram from 'node:dgram';
 import { Buffer } from 'node:buffer';
 import util from 'util';
 
+import dns from 'dns';
 import { lookup } from 'node:dns/promises';
 
 const host = 'www.example.com';
@@ -10,17 +11,24 @@ const ip = await lookup(host);
 const socket = dgram.createSocket({
   type: 'udp4',
   lookup: (hostname, options, callback) => {
-    console.log('called dns.lookup', { hostname, options });
-    callback(null, ip.address, ip.family);
+    if (hostname === host) {
+      callback(null, ip.address, ip.family);
+      return;
+    }
+
+    console.log('calling dns.lookup', { hostname });
+    dns.lookup(hostname, options, callback);
   },
 });
 socket.send2 = util.promisify(socket.send);
 
 const msg = Buffer.from('foo');
 
-await socket.send2(msg, 0, msg.length, 8125, ip.address);
+await socket.send2(msg, 0, msg.length, 8125, host);
 console.log('msg 1 sent');
-await socket.send2(msg, 0, msg.length, 8125, ip.address);
+await socket.send2(msg, 0, msg.length, 8125, host);
 console.log('msg 2 sent');
+await socket.send2(msg, 0, msg.length, 8125, 'example.net');
+console.log('msg 3 sent');
 
 socket.close();
